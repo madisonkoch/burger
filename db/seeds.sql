19403c823a2de4e90258a170e08ddad6b128f499
madisonkoch <kochx348@umn.edu>
2018-05-15 19:24:01 -0500

    folder/file setup is done

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger!');
+INSERT INTO burgers (burger_name) VALUES ('Double bacon avocado burger');
+INSERT INTO burgers (burger_name) VALUES ('Garden chicken burger');
