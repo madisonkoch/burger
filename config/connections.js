14f4a42e9e6c23ca6c65ed88e3eba9d5c314159d
Koch <kochm@vikings.nfl.net>
2018-05-22 18:34:16 -0500

    add jawsDB code in connections.js

--- config/connections.js
+++ config/connections.js
@@ -1,12 +1,19 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "#Rowgophs8",
-  database: "burgers_db"
-});
+var connection;
+
+//Connection declaration (including JawsDB)
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "#Rowgophs8",
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
